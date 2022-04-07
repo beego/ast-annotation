5850118768d2d7a6050d71d37f19d3db07c8c44b
Deng Ming <mingflycash@gmail.com>
2022-04-07 23:35:14 +0800

    single file annotation

--- annotation.go
+++ annotation.go
@@ -19,13 +19,9 @@ import (
 	"strings"
 )
 
-type Annotations struct {
+type annotations struct {
 	Node ast.Node
-	Ans  map[string]Annotation
-}
-
-type Node struct {
-	Node ast.Node
+	Ans  []Annotation
 }
 
 type Annotation struct {
@@ -33,53 +29,11 @@ type Annotation struct {
 	Value string
 }
 
-type FieldAnnotationVisitor interface {
-	Visit(field *ast.Field, ans Annotations)
-}
-
-type FuncDeclAnnotationVisitor interface {
-	Visit(fn *ast.FuncDecl, ans Annotations)
-}
-
-type FileAnnotationVisitor interface {
-	Visit(f *ast.File, ans Annotations)
-}
-
-type TypeAnnotationVisitor interface {
-	Visit(s *ast.TypeSpec, ans Annotations)
-}
-
-type AnnotationVisitor struct {
-	File     FileAnnotationVisitor
-	FuncDecl FuncDeclAnnotationVisitor
-	Field    FieldAnnotationVisitor
-	Type     TypeAnnotationVisitor
-}
-
-func (a *AnnotationVisitor) Visit(node ast.Node) (w ast.Visitor) {
-	switch n := node.(type) {
-	case *ast.File:
-		if n.Doc != nil {
-			a.File.Visit(n, newAnnotations(n, n.Doc))
-		}
-	case *ast.Field:
-		if n.Doc != nil {
-			a.Field.Visit(n, newAnnotations(n, n.Doc))
-		}
-	case *ast.FuncDecl:
-		if n.Doc != nil {
-			a.FuncDecl.Visit(n, newAnnotations(n, n.Doc))
-		}
-	case *ast.TypeSpec:
-		if n.Doc != nil {
-			a.Type.Visit(n, newAnnotations(n, n.Doc))
-		}
+func newAnnotations(n ast.Node, cg *ast.CommentGroup) annotations {
+	if cg == nil || len(cg.List) == 0 {
+		return annotations{Node: n}
 	}
-	return a
-}
-
-func newAnnotations(n ast.Node, cg *ast.CommentGroup) Annotations {
-	ans := make(map[string]Annotation)
+	ans := make([]Annotation, 0, len(cg.List))
 	for _, c := range cg.List {
 		text, ok := extractContent(c)
 		if !ok {
@@ -91,13 +45,13 @@ func newAnnotations(n ast.Node, cg *ast.CommentGroup) Annotations {
 				continue
 			}
 			key := segs[0][1:]
-			ans[key] = Annotation{
+			ans = append(ans, Annotation{
 				Key:   key,
 				Value: segs[1],
-			}
+			})
 		}
 	}
-	return Annotations{
+	return annotations{
 		Node: n,
 		Ans:  ans,
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/beego/ast-annotation
 
 go 1.14
 
-require github.com/stretchr/testify v1.7.1
+require (
+	github.com/stretchr/testify v1.7.1
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
